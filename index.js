5d66b1733664ef0e2fb968f16cedc693d0d7b6e1
denis <denisskholmov@gmail.com>
2025-06-24 12:36:13 +0300

    добавил spinner

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function render() {
   productsPage.render();
 }
 
+spinnerPage.render();
+
 let CATALOG = [];
 
 // myJson: https://www.myjsons.com/v/47254233
@@ -12,6 +14,7 @@ fetch('server/catalog.json')
   .then(res => res.json())
   .then(body => {
     CATALOG = body;
+    spinnerPage.handleClear();
     render();
   })
   .catch(error => {
